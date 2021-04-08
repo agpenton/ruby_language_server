dc4f62d2f2e194a462d44d0d85022394faa9323a
Asdrubal Gonzalez <agpenton@gmail.com>
2021-04-08 20:07:28 +0400

    adding more workflows to the acctions

--- lib/ruby_language_server/logger.rb
+++ lib/ruby_language_server/logger.rb
@@ -9,6 +9,6 @@ module RubyLanguageServer
   class << self
     attr_accessor :logger
   end
-  @logger = ::Logger.new(STDERR, level: level)
+  @logger = ::Logger.new($stderr, level: level)
   @logger.log(level, "Logger started at level #{level_name} -> #{level}")
 end
