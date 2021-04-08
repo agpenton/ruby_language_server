95c5573f7dc8e3e3fcae568af5a49ed6aea8a038
Asdrubal Gonzalez <agpenton@gmail.com>
2021-04-08 20:00:15 +0400

    adding more workflows to the acctions

--- lib/ruby_language_server/io.rb
+++ lib/ruby_language_server/io.rb
@@ -15,8 +15,8 @@ module RubyLanguageServer
       configure_io
 
       loop do
-        (id, response) = process_request
-        return_response(id, response) unless id.nil?
+        (id, response) = process_request($stdin)
+        return_response(id, response, $stdout) unless id.nil?
       rescue SignalException => e
         RubyLanguageServer.logger.error "We received a signal.  Let's bail: #{e}"
         exit
@@ -34,8 +34,21 @@ module RubyLanguageServer
       end
     end
 
-    def send_notification(message, params)
-      io ||= out
+    def return_response(id, response, io = $stdout)
+      full_response = {
+        jsonrpc: '2.0',
+        id: id,
+        result: response
+      }
+      response_body = JSON.unparse(full_response)
+      RubyLanguageServer.logger.info "return_response body: #{response_body}"
+      io.write "Content-Length: #{response_body.length + 0}\r\n"
+      io.write "\r\n"
+      io.write response_body
+      io.flush
+    end
+
+    def send_notification(message, params, io = $stdout)
       full_response = {
         jsonrpc: '2.0',
         method: message,
@@ -49,43 +62,8 @@ module RubyLanguageServer
       io.flush if io.respond_to?(:flush)
     end
 
-    private
-
-    attr_accessor :in, :out
-
-    def configure_io
-      if ENV['LSP_PORT']
-        @tcp_server = TCPServer.new(ENV['LSP_PORT'].to_i)
-        RubyLanguageServer.logger.info "Listening on TCP port #{ENV['LSP_PORT']} for LSP connections"
-        self.in = @tcp_server.accept
-        self.out = self.in
-        @using_socket = true
-        RubyLanguageServer.logger.info 'Accepted LSP socket connection'
-      else
-        self.in = $stdin
-        self.out = $stdout
-        @using_socket = false
-      end
-    end
-
-    def return_response(id, response)
-      io ||= out
-      full_response = {
-        jsonrpc: '2.0',
-        id:,
-        result: response
-      }
-      response_body = JSON.generate(full_response)
-      RubyLanguageServer.logger.info "return_response body: #{response_body}"
-      io.write "Content-Length: #{response_body.length}\r\n"
-      io.write "\r\n"
-      io.write response_body
-      io.flush if io.respond_to?(:flush)
-    end
-
-    def process_request
-      request_body = get_request
-      # RubyLanguageServer.logger.debug "request_body: #{request_body}"
+    def process_request(io = $stdin)
+      request_body = get_request(io)
       request_json = JSON.parse request_body
       id = request_json['id']
       method_name = request_json['method']
@@ -111,8 +89,8 @@ module RubyLanguageServer
       end
     end
 
-    def get_request # rubocop:disable Naming/AccessorMethodName
-      initial_line = get_initial_request_line
+    def get_request(io = $stdin)
+      initial_line = get_initial_request_line(io)
       RubyLanguageServer.logger.debug "initial_line: #{initial_line}"
       length = get_length(initial_line)
       content = ''
@@ -129,8 +107,8 @@ module RubyLanguageServer
       content
     end
 
-    def get_initial_request_line # rubocop:disable Naming/AccessorMethodName
-      self.in.gets
+    def get_initial_request_line(io = $stdin)
+      io.gets
     end
 
     def get_length(string)
@@ -139,8 +117,36 @@ module RubyLanguageServer
       string.match(/Content-Length: (\d+)/)[1].to_i
     end
 
-    def get_content(size)
-      self.in.read(size)
+    def get_content(size, io = $stdin)
+      io.read(size)
     end
+    # http://www.alecjacobson.com/weblog/?p=75
+    # def $stdin_read_char
+    #   begin
+    #     # save previous state of stty
+    #     old_state = `stty -g`
+    #     # disable echoing and enable raw (not having to press enter)
+    #     system "stty raw -echo"
+    #     c = $stdin.getc.chr
+    #     # gather next two characters of special keys
+    #     if(c=="\e")
+    #       extra_thread = Thread.new{
+    #         c = c + $stdin.getc.chr
+    #         c = c + $stdin.getc.chr
+    #       }
+    #       # wait just long enough for special keys to get swallowed
+    #       extra_thread.join(0.00001)
+    #       # kill thread so not-so-long special keys don't wait on getc
+    #       extra_thread.kill
+    #     end
+    #   rescue Exception => ex
+    #     puts "#{ex.class}: #{ex.message}"
+    #     puts ex.backtrace
+    #   ensure
+    #     # restore previous state of stty
+    #     system "stty #{old_state}"
+    #   end
+    #   return c
+    # end
   end # class
 end # module
