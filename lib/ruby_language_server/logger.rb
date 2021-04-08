8f11db6aaf3766306be5ea9b8e43be631ca60dbc
Asdrubal Gonzalez <agpenton@gmail.com>
2021-04-08 20:16:00 +0400

    adding more workflows to the acctions

--- lib/ruby_language_server/logger.rb
+++ lib/ruby_language_server/logger.rb
@@ -3,7 +3,8 @@
 require 'logger'
 
 module RubyLanguageServer
-  level_name = ENV.fetch('LOG_LEVEL', 'error').upcase || 'DEBUG'
+  level_name = ENV.fetch('LOG_LEVEL', 'error').upcase
+  # level_name = 'DEBUG'
   level = Logger::Severity.const_get(level_name)
   class << self
     attr_accessor :logger
